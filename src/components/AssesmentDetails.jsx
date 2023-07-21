522b9abbe40dd131f2591b9a32fe7d0f89fe362e
unknown <harshit.nitmz@gmail.com>
2023-07-21 06:15:39 +0530

    add My assignment section

---
+++ src/components/AssesmentDetails.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const AssesmentDetails = () => {
+  return (
+    <div className="maths-assesment">
+          <div className="maths-box-1">
+            <img alt="icon" src="/assets/briefcase_image.svg" />
+            <span>Maths Assesment</span>
+    <img style={{position:"absolute" ,right:"16px",top:"16px"}} src='/assets/3 dot.svg'/> 
+    {/*have already set maths-assesment postion to relative*/}
+            <div className="maths-calendar">
+              <span>Job</span>
+              <img alt="vector" src="/assets/Vector 333.svg" />
+              <img alt="calender" src="/assets/calendar_today.svg" />
+
+              <time >20 Apr 2023</time>
+            </div>
+          </div>
+            <img src="/assets/Vector 332.svg" alt="vector"/>
+          <div className="maths-footer">
+            <div className="ques-duration">
+              <div><p>00</p><p>Duration</p></div>
+              <div><p>00</p><p>Questions</p></div>
+            
+            </div>
+            <div className="share-user">
+                <div className="share-image"><img src="/assets/link.svg"/>Share</div>
+                <img src="\assets\User_LP.svg" alt="user"/>
+            </div>
+          </div>
+        </div>
+        
+      
+  )
+}
+
+export default AssesmentDetails
--- src/components/MyAssesments.jsx
+++ src/components/MyAssesments.jsx
@@ -1,10 +1,25 @@
 import React from "react";
+import AssesmentDetails from "./AssesmentDetails";
+
 
 const MyAssesments = () => {
   return (
     <section className="my-assesments">
-      <h1>My Assesment</h1>
-      <div></div>
+      <p>My Assesment</p>
+      <div className="assesment-cards">
+        <div className="new-assesment">
+          <img alt="icon" src="/assets/add.svg" />
+          <span>New Assesment</span>
+
+          <p>
+            From here you can add questions of multiple types like MCQs,
+            subjective (text or paragraph)!
+          </p>
+        </div>
+       <AssesmentDetails />
+      <AssesmentDetails />
+        
+      </div>
       <div></div>
     </section>
   );
